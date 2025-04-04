99c2990bcd1a923ffcf4dd2971ebfa5c84e1e2a5
Lilian Brosset <lilian.bross85700@gmail.com>
2025-04-04 10:04:46 +0200

    Commit 5

--- TP SITE E-commerce/SITE E-commerce.sql
+++ TP SITE E-commerce/SITE E-commerce.sql
@@ -11,8 +11,8 @@ CREATE TABLE article(
 
 CREATE TABLE client(
 	id INT NOT NULL AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL,
     prenom VARCHAR(50) NOT NULL,
+    nom VARCHAR(50) NOT NULL,
     CONSTRAINT pk_client PRIMARY KEY (id)
 ) ENGINE = Innodb;
 
@@ -22,18 +22,19 @@ CREATE TABLE commande (
     date_achat DATETIME,
     CONSTRAINT pk_commande PRIMARY KEY (id),
     FOREIGN KEY (client_id) REFERENCES client(id)
-);
+) ENGINE = Innodb;
 
 CREATE TABLE commande_article (
     id INT NOT NULL AUTO_INCREMENT,
     commande_id INT,
     article_id INT,
     nb INT,
+    prix_unitaire INT,
     total DECIMAL(10,2),
 	CONSTRAINT pk_commande_article PRIMARY KEY (id),
     FOREIGN KEY (commande_id) REFERENCES commande(id),
     FOREIGN KEY (article_id) REFERENCES article(id)
-);
+) ENGINE = Innodb;
 
 INSERT INTO article(id, nom, prix) VALUES
 (1 ,'PlayStation 5', 400),
@@ -41,8 +42,30 @@ INSERT INTO article(id, nom, prix) VALUES
 (3 ,'Machine à café', 300),
 (4 ,'PlayStation 3', 100);
 
-INSERT INTO clients(id, nom, prenom) VALUES
+INSERT INTO client(id, prenom, nom) VALUES
 (1, 'Brad' , 'PITT'),
 (2, 'George', 'CLOONEY'),
 (3, 'Jean', 'DUJARDIN');
 
+INSERT INTO commande (id, client_id, date_achat) VALUES
+(1, 1, '2024-09-08 10:15:00');
+
+INSERT INTO commande_article (commande_id, article_id, nb, prix_unitaire, total) VALUES
+(1, 2, 1, 350.00, 350.00),  
+(1, 3, 1, 300.00, 300.00),  
+(1, 4, 2, 100.00, 200.00);  
+
+-- 4 --
+SELECT 
+    client.nom AS nom, 
+    client.prenom AS prenom, 
+    commande.date_achat, 
+    article.nom AS article, 
+    commande_article.prix_unitaire AS prix, 
+    commande_article.nb, 
+    commande_article.total
+FROM commande 
+JOIN client ON commande.client_id = client.id
+JOIN commande_article ON commande.id = commande_article.commande_id
+JOIN article  ON commande_article.article_id = article.id
+WHERE client.nom = 'PITT' AND client.prenom = 'Brad';
